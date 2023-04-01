b4daa331cf25f831adff4dda8276be6de36894ac
KennethJHan <kenneth.jh.han@gmail.com>
2023-04-01 15:07:08 +0900

    add rna_to_ptn_translate_table

--- src/util.h
+++ src/util.h
@@ -1,10 +1,12 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <map>
 #include <string>
 
 void reverse_string(std::string &str);
 void complement_dna(std::string &str);
 double calc_gc(std::string &str);
 int calc_hamming_distance(std::string &str1, std::string &str2);
+std::string &translate_rna_to_protein(std::string &str);
 #endif
